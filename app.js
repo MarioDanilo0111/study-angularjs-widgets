0ef02ea6f270886b191b0ed108121a979588ed41
Mario Fernández <nilovinilo1@gmail.com>
2026-02-08 12:54:34 +0100

    feat: Learing about 'ng-module', how it binds object and how that makes form feel alive  is a shared object between controller and HTML, adding something on , then the 'template' can see it if the 'template' changes it the controller sees it.

--- app.js
+++ app.js
@@ -2,4 +2,19 @@ var app = angular.module("studyApp", []);
 
 app.controller("MainCtrl", function ($scope) {
   $scope.message = "Hello AngularJs";
+
+  $scope.form = {
+    name: "",
+  };
+
+  //use input->"name" value in Html form
+  $scope.submit = function () {
+    if ($scope.form.name) {
+      $scope.result = "Hello " + $scope.form.name;
+    }
+  };
+
+  $scope.add = function () {
+    $scope.message = "Button clicked";
+  };
 });
