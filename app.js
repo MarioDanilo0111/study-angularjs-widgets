942af8e2d02db89746e4c41a81ef90a94336d219
Mario Fernández <nilovinilo1@gmail.com>
2026-02-08 13:55:00 +0100

    Kata 03: ng-repet list with add/remove and empty state, conditional controller(ng-if)

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 var app = angular.module("studyApp", []);
 
 app.controller("MainCtrl", function ($scope) {
+  //list state
+  $scope.items = [];
+  // Form state
+  $scope.newItem = "";
+  // Add item
+  $scope.addItem = function () {
+    if (!$scope.newItem) return;
+
+    $scope.items.push({
+      name: $scope.newItem,
+    });
+    $scope.newItem = "";
+  };
+
+  // Remove item
+  $scope.removeItem = function (index) {
+    $scope.items.splice(index, 1);
+  };
+
   $scope.message = "Hello AngularJs";
 
   $scope.form = {
